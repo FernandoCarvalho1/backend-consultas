a4b694b93c3fc9676a9083494d270bef5d6daac6
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-27 09:01:38 -0300

    feat: criação da camada service de Especialidade

---
+++ src/main/java/com/fiap/ec/service/EspecialidadeService.java
@@ -0,0 +1,18 @@
+package com.fiap.ec.service;
+import com.fiap.ec.model.Especialidade;
+import com.fiap.ec.repository.EspecialidadeRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class EspecialidadeService {
+    private final EspecialidadeRepository repository;
+    public EspecialidadeService(EspecialidadeRepository repository) {
+        this.repository = repository;
+    }
+    public Especialidade salvar(Especialidade especialidade) {
+        return repository.save(especialidade);
+    }
+    public List<Especialidade> listar() {
+        return repository.findAll();
+    }
+}
