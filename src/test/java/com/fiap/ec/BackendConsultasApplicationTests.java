b7ceef08d5354cb23335a9811afc9394eb3e0cd3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-27 08:52:49 -0300

    chore: estrutura inicial gerada pelo Spring Initializr

---
+++ src/test/java/com/fiap/ec/BackendConsultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendConsultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
